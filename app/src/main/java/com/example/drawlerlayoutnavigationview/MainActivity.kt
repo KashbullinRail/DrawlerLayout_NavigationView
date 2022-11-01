82f89de65862ec812d0726e4218e95349ea193b5
KashbullinRail <kashbullinrail@gmail.com>
2022-11-01 22:10:39 +0300

    NavigationView закончен, созданы функции нажатия иконок

--- app/src/main/java/com/example/drawlerlayoutnavigationview/MainActivity.kt
+++ app/src/main/java/com/example/drawlerlayoutnavigationview/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.drawlerlayoutnavigationview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.core.view.GravityCompat
 import com.example.drawlerlayoutnavigationview.databinding.ActivityMainBinding
 
@@ -10,8 +11,20 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+
         setContentView(binding.root)
         binding.apply {
+            navView.setNavigationItemSelectedListener {
+                when (it.itemId) {
+                    R.id.item1 -> Toast.makeText(this@MainActivity, "Item 1", Toast.LENGTH_SHORT).show()
+                    R.id.item21 -> {
+                        Toast.makeText(this@MainActivity, "Item 21", Toast.LENGTH_SHORT).show()
+                        Toast.makeText(this@MainActivity, "Item 2121", Toast.LENGTH_SHORT).show()
+                    }
+                }
+                drawer.closeDrawer(GravityCompat.START)
+                true
+            }
             btnOpen.setOnClickListener {
                 drawer.openDrawer(GravityCompat.START)
             }
